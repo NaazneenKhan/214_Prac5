53b569a6a01693bce1a041b04ce09822e5452a71
Tino Chirozvi <134059951+tinothecreator@users.noreply.github.com>
2024-10-05 15:50:55 +0200

    test before you push

---
+++ main.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+
+
+void TestComponent1 (){
+
+}
+
+void TestComponent2 (){
+    
+}
+
+void TestComponent3(){
+    
+}
+
+void TestComponent4 (){
+    
+}
+
+int main(){
+   
+   TestComponent1();
+   TestComponent2();
+   TestComponent3();
+   TestComponent4();
+
+
+    return 0;
+}
